d95666a953935722a430148f19dec7ecb692e363
0vitor <w.wutaes@gmail.com>
2025-11-18 21:21:02 -0300

    Primeiro commit

---
+++ TodoApi/TodoModule/TodoDto.cs
@@ -0,0 +1,12 @@
+﻿public class TodoDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public bool IsComplete { get; set; }
+
+    public TodoDto() { }
+    public TodoDto(Todo todo)
+    {
+    (Id, Name, IsComplete) = (todo.Id, todo.Name, todo.IsComplete);
+    }
+}
